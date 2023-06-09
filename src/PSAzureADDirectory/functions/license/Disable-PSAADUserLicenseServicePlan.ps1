2ce16113920f4a8c67ec63215ac600cb01f552ec
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2023-06-09 12:53:30 +0200

    Test

--- src/PSAzureADDirectory/functions/license/Disable-PSAADUserLicenseServicePlan.ps1
+++ src/PSAzureADDirectory/functions/license/Disable-PSAADUserLicenseServicePlan.ps1
@@ -27,6 +27,14 @@
     
     .PARAMETER WhatIf
         Enables the function to simulate what it will do instead of actually executing.
+    
+    .PARAMETER Confirm
+        The Confirm switch instructs the command to which it is applied to stop processing before any changes are made.
+        The command then prompts you to acknowledge each action before it continues.
+        When you use the Confirm switch, you can step through changes to objects to make sure that changes are made only to the specific objects that you want to change.
+        This functionality is useful when you apply changes to many objects and want precise control over the operation of the Shell.
+        A confirmation prompt is displayed for each object before the Shell modifies the object.
+
 
 
 	.EXAMPLE
