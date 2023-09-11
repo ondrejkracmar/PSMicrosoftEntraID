74c6e524059dbb2085512b640c4e4a97b6da6d0f
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2023-09-11 13:06:16 +0200

    Update New-PSEntraIDInvitation

--- src/PSMicrosoftEntraID/functions/user/New-PSEntraIDInvitation.ps1
+++ src/PSMicrosoftEntraID/functions/user/New-PSEntraIDInvitation.ps1
@@ -13,22 +13,31 @@
         DIsplayName, GivenName, SureName of the user attribute populated in tenant/directory.
 
     .PARAMETER InviteRedirectUrl
-        CompanyName of the user attribute populated in tenant/directory.
+        The URL that the user will be redirected to after redemption.
+    
+    .PARAMETER SendInvitationMessage
+        The invitation message        
 
     .PARAMETER MessageLanguage
-        Return disabled accounts in tenant/directory.
+        Langueage of invite message.
 
-    .PARAMETER Filter
-        Filter expressions of accounts in tenant/directory.
+    .PARAMETER CCRecipient
+        Name and mail of CC recipients
 
-    .PARAMETER AdvancedFilter
-        Switch advanced filter for filtering accounts in tenant/directory.
+    .PARAMETER EnableException
+        This parameters disables user-friendly warnings and enables the throwing of exceptions. This is less user frien
+        dly, but allows catching exceptions in calling scripts.
 
-    .PARAMETER All
-        Return all accounts in tenant/directory.
+    .PARAMETER WhatIf
+        Enables the function to simulate what it will do instead of actually executing.
 
-    .PARAMETER PageSize
-        Value of returned result set contains multiple pages of data.
+    .PARAMETER Confirm
+        The Confirm switch instructs the command to which it is applied to stop processing before any changes are made.
+        The command then prompts you to acknowledge each action before it continues.
+        When you use the Confirm switch, you can step through changes to objects to make sure that changes are made only to the specific objects that you want to change.
+        This functionality is useful when you apply changes to many objects and want precise control over the operation of the Shell.
+        A confirmation prompt is displayed for each object before the Shell modifies the object.
+
 
     .EXAMPLE
         PS C:\> Get-PSEntraIDUser -Identity user1@contoso.com
@@ -41,7 +50,7 @@
     [CmdletBinding(DefaultParameterSetName = 'UserEmailAddres')]
     param (
         [Parameter(Mandatory = $True, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'UserEmailAddress')]
-        [ValidateEmailAddres()]
+        [ValidateMailAddress()]
         [string]
         [Alias("UserEmailAddress", "EmailAddres", "Mail","UserPrincipalName","InvitedUserPrincipalName")]
         $InvitedUserEmailAddress,
