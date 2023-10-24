706952d6b67eec6d4fbed61fae2aa6c8424e3803
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2023-10-24 13:40:22 +0200

    Build new version 0.5,1.5

---
+++ src/PSMicrosoftEntraID/internal/functions/subscription/Register-PSEntraIDSubscribedSku.ps1
@@ -0,0 +1,40 @@
+﻿function Register-PSEntraIDSubscribedSku {
+    <#
+	.SYNOPSIS
+		Register the list of commercial subscriptions that an organization has acquired.
+
+	.DESCRIPTION
+		Register the list of commercial subscriptions that an organization has acquired.
+
+    .PARAMETER EnableException
+        This parameters disables user-friendly warnings and enables the throwing of exceptions. This is less user friendly,
+        but allows catching exceptions in calling scripts.
+
+	.EXAMPLE
+		PS C:\> Register-PSEntraIDSubscribedSku
+
+		Register the list of commercial subscriptions
+
+	#>
+    [OutputType('PSMicrosoftEntraID.License')]
+    [CmdletBinding()]
+    param (
+
+    )
+    begin {
+        Assert-RestConnection -Service graph -Cmdlet $PSCmdlet
+        $query = @{
+            '$select' = ((Get-PSFConfig -Module $sript:ModuleName -Name Settings.GraphApiQuery.Select.SubscribedSku).Value -join ',')
+        }
+        $commandRetryCount = Get-PSFConfigValue -FullName ('{0}.Settings.Command.RetryCount' -f $script:ModuleName)
+        $commandRetryWait = New-TimeSpan -Seconds (Get-PSFConfigValue -FullName ('{0}.Settings.Command.RetryWaitIsSeconds' -f $script:ModuleName))
+    }
+    process {
+        Invoke-PSFProtectedCommand -ActionString 'SubscribedSku.List' -Target (Get-PSFLocalizedString -Module $script:ModuleName -Name Identity.Platform) -ScriptBlock {
+            Invoke-RestRequest -Service 'graph' -Path subscribedSkus -Query $query -Method Get -ErrorAction Stop | ConvertFrom-RestSubscribedSku
+        } -EnableException $EnableException -PSCmdlet $PSCmdlet -Continue -RetryCount $commandRetryCount -RetryWait $commandRetryWait | Set-PSFResultCache
+        if (Test-PSFFunctionInterrupt) { return }
+    }
+    end
+    {}
+}
