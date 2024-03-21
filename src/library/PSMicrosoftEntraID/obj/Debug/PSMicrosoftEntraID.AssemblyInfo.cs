cc2c7da250843070db960ad31f8328e92e7b5a32
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2024-03-21 13:45:41 +0100

    cahce

--- src/library/PSMicrosoftEntraID/obj/Debug/PSMicrosoftEntraID.AssemblyInfo.cs
+++ src/library/PSMicrosoftEntraID/obj/Debug/PSMicrosoftEntraID.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PSMicrosoftEntraID")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de4dd899cf2e01ac36c18a81ca59bffb69631a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca1b0374986fe21b17bc2f2850c3344b874cefb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PSMicrosoftEntraID")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PSMicrosoftEntraID")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
