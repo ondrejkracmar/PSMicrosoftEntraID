e501ffe6f55e358e3a8d6b32d1a7955943635a38
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2024-04-09 14:55:17 +0200

    Move folder with test

--- src/library/PSMicrosoftEntraID/obj/Debug/PSMicrosoftEntraID.AssemblyInfo.cs
+++ src/library/PSMicrosoftEntraID/obj/Debug/PSMicrosoftEntraID.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PSMicrosoftEntraID")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2c7da250843070db960ad31f8328e92e7b5a32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d130d103a0884c4c86efda342efd4213afeecc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PSMicrosoftEntraID")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PSMicrosoftEntraID")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
