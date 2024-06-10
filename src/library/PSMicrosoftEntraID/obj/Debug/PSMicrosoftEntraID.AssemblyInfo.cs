9acf902354911e93e52ff9a538159d95e860eeea
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2024-06-10 10:27:52 +0200

    Update container definition

--- src/library/PSMicrosoftEntraID/obj/Debug/PSMicrosoftEntraID.AssemblyInfo.cs
+++ src/library/PSMicrosoftEntraID/obj/Debug/PSMicrosoftEntraID.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PSMicrosoftEntraID")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d8886d41736c79fa090f4575939e2f241912e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caf4322d5f9d776cd751ece7194841fac6c6efdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PSMicrosoftEntraID")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PSMicrosoftEntraID")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
