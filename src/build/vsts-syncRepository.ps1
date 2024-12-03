ed733e8fa6d2b4444f34b0e0a8ff178d3ddb122e
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2024-12-03 09:54:19 +0000

    Fix issuew with vsts-syncRepository.ps1

--- src/build/vsts-syncRepository.ps1
+++ src/build/vsts-syncRepository.ps1
@@ -9,12 +9,12 @@
 
 try {
     # Construct the Azure DevOps and GitHub repository URLs
-    $AzureRepoUrl = 'https://dev.azure.com/{0}/{1}/_git/{2}]' -f $AzureDevOpsOrganizationName, $AzureDevOpsProjectName, $AzureDevOpsRepositoryName
-    $GitHubRepoUrl = 'github.com/{0}/{1}' -f $GitHubUsername, $GitHubRepositoryName
+    $AzureRepoUrl = ('https://dev.azure.com/{0}/{1}/_git/{2}]' -f $AzureDevOpsOrganizationName, $AzureDevOpsProjectName, $AzureDevOpsRepositoryName)
+    $GitHubRepoUrl = 'github.com/{0}/{1}' -f ($GitHubUsername, $GitHubRepositoryName)
 
     # Log the constructed URLs
-    Write-PSFMessage -Level Host -Message 'Azure DevOps Repository URL: {0}' -f $AzureRepoUrl
-    Write-PSFMessage -Level Host -Message 'GitHub Repository URL: ' -f $GitHubRepoUrl
+    Write-PSFMessage -Level Host -Message ('Azure DevOps Repository URL: {0}' -f $AzureRepoUrl)
+    Write-PSFMessage -Level Host -Message ('GitHub Repository URL: ' -f $GitHubRepoUrl)
 
     # Clone the Azure DevOps repository in mirror mode
     Write-PSFMessage -Level Host -Message "Cloning Azure DevOps repository..."
@@ -25,7 +25,7 @@ try {
 
     # Add GitHub as a remote repository
     Write-PSFMessage -Level Host -Message "Adding GitHub remote repository..."
-    git remote add github 'https://{0}@{1}' -f $GitHubToken, $GitHubRepoUrl
+    git remote add github ('https://{0}@{1}' -f $GitHubToken, $GitHubRepoUrl)
 
     # Push changes to GitHub
     Write-PSFMessage -Level Host -Message "Pushing to GitHub..."
